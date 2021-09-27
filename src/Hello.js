f89b8466bea4bd9089d0e470d59eef8fda4a64a9
mzy0125 <mzy0125@outlook.com>
2021-09-27 09:22:10 +0800

    import Hello component

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Hello(props){
+  return(
+    <div>
+      <div>this is Hello component</div>
+      <h1>I am h1</h1>
+      <h6>I am h6</h6>
+    </div>
+  )
+}
+
+export default Hello
