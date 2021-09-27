06a5fc78fee6f484d89f72b252e0bdacdd3f8fb2
mzy0125 <mzy0125@outlook.com>
2021-09-27 09:18:02 +0800

    learn Components and Props

--- src/index.js
+++ src/index.js
@@ -1,15 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from "./Hello";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName
+}
+
+const user = {
+  firstName: 'Peter',
+  lastName: 'Parker'
+};
+
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+function People(props) {
+  return (
+    <div>
+      <h1>name:{props.name} age:{props.age}</h1>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Alice"/>
+      <Welcome name="Bob"/>
+      <Welcome name="Clark"/>
+      <h1>formatName(user)</h1>
+      <People name={"alibaba"} age={20}/>
+      <Hello></Hello>
+      <Clock date={new Date()}/>
+    </div>
+  )
+}
+
+function Clock(props) {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {props.date.toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+function tick() {
+  ReactDOM.render(
+    <App className="test"/>,
+    document.getElementById('root')
+  );
+}
+
+setInterval(tick, 1000);
+
+const data = ['A', 'B', 'C']
+const VDOM = (
+  <div>
+    <ul>
+      {
+        data.map((item, index) => {
+          return <li key={index}>{item}</li>
+        })
+      }
+    </ul>
+  </div>
+)
+ReactDOM.render(VDOM, document.querySelector('#root'))
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
